76e0acb54558965b80c2ac4928e3aec2c6fb03f8
00011095 <wiut00011095@gmail.com>
2024-10-31 15:50:26 +0500

    Db contect config

---
+++ DSCC_CW1_11095_Back/Data/ProductContext.cs
@@ -0,0 +1,23 @@
+﻿using DSCC_CW1_11095_Back.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DSCC_CW1_11095_Back.Data
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Producer> Producers { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .HasOne(p => p.Producer)
+                .WithMany(pr => pr.Products)
+                .HasForeignKey(p => p.ProducerId);
+        }
+    }
+}
