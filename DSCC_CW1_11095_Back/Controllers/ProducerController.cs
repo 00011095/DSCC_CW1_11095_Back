c66b66bb21f12c59fe914bbcc5ac8a222ee84175
00011095 <wiut00011095@gmail.com>
2024-10-31 15:51:28 +0500

    Controllers

---
+++ DSCC_CW1_11095_Back/Controllers/ProducerController.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using DSCC_CW1_11095_Back.Dtos;
+using DSCC_CW1_11095_Back.Interfaces;
+using DSCC_CW1_11095_Back.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DSCC_CW1_11095_Back.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProducerController : ControllerBase
+    {
+        private readonly IProducerRepository _producerRepository;
+        private readonly IMapper _mapper;
+
+        public ProducerController(IProducerRepository producerRepository, IMapper mapper)
+        {
+            _producerRepository = producerRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProducerDto>>> GetAllProducers()
+        {
+            var producers = await _producerRepository.GetAllAsync();
+            var producerDtos = _mapper.Map<IEnumerable<ProducerDto>>(producers);
+            return Ok(producerDtos);
+        }
+
+        [HttpGet("{id}", Name = "GetProducerById")]
+        public async Task<ActionResult<ProducerDto>> GetProducerById(int id)
+        {
+            var producer = await _producerRepository.GetByIdAsync(id);
+            if (producer == null) return NotFound();
+            return Ok(_mapper.Map<ProducerDto>(producer));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProducerDto>> CreateProducer(ProducerCreateDto producerCreateDto)
+        {
+            var producer = _mapper.Map<Producer>(producerCreateDto);
+            await _producerRepository.AddAsync(producer);
+
+            var producerDto = _mapper.Map<ProducerDto>(producer);
+            return CreatedAtRoute("GetProducerById", new { id = producerDto.Id }, producerDto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProducer(int id, ProducerCreateDto producerUpdateDto)
+        {
+            var producer = await _producerRepository.GetByIdAsync(id);
+            if (producer == null) return NotFound();
+
+            _mapper.Map(producerUpdateDto, producer);
+            await _producerRepository.UpdateAsync(producer);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProducer(int id)
+        {
+            var producer = await _producerRepository.GetByIdAsync(id);
+            if (producer == null) return NotFound();
+
+            await _producerRepository.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
---
+++ DSCC_CW1_11095_Back/Controllers/ProductController.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using DSCC_CW1_11095_Back.Dtos;
+using DSCC_CW1_11095_Back.Interfaces;
+using DSCC_CW1_11095_Back.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DSCC_CW1_11095_Back.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+
+        public ProductController(IProductRepository productRepository, IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetAllProducts()
+        {
+            var products = await _productRepository.GetAllAsync();
+            var productDtos = _mapper.Map<IEnumerable<ProductDto>>(products);
+            return Ok(productDtos);
+        }
+
+        [HttpGet("{id}", Name = "GetProductById")]
+        public async Task<ActionResult<ProductDto>> GetProductById(int id)
+        {
+            var product = await _productRepository.GetByIdAsync(id);
+            if (product == null) return NotFound();
+            return Ok(_mapper.Map<ProductDto>(product));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProductDto>> CreateProduct(ProductCreateDto productCreateDto)
+        {
+            var product = _mapper.Map<Product>(productCreateDto);
+            await _productRepository.AddAsync(product);
+
+            var productDto = _mapper.Map<ProductDto>(product);
+            return CreatedAtRoute("GetProductById", new { id = productDto.Id }, productDto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProduct(int id, ProductCreateDto productUpdateDto)
+        {
+            var product = await _productRepository.GetByIdAsync(id);
+            if (product == null) return NotFound();
+
+            _mapper.Map(productUpdateDto, product);
+            await _productRepository.UpdateAsync(product);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await _productRepository.GetByIdAsync(id);
+            if (product == null) return NotFound();
+
+            await _productRepository.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
