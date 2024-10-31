bbd1be8a5bca465f842ff108de337b10b323f6eb
00011095 <wiut00011095@gmail.com>
2024-10-31 15:50:48 +0500

    Mapping with migration

---
+++ DSCC_CW1_11095_Back/Mapping/MappingProfile .cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using DSCC_CW1_11095_Back.Dtos;
+using DSCC_CW1_11095_Back.Models;
+
+namespace DSCC_CW1_11095_Back.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Producer, ProducerDto>();
+            CreateMap<ProducerCreateDto, Producer>();
+            CreateMap<Product, ProductDto>();
+            CreateMap<ProductCreateDto, Product>();
+        }
+    }
+}
